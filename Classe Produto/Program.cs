037b8bb816dffec4adb04ccfbb3a757030f999ea
vitor <vitor@DESKTOP-4OIHHQ9>
2023-04-26 16:08:53 -0300

    Add project files.

---
+++ Classe Produto/Program.cs
@@ -0,0 +1,32 @@
+﻿using Classe_Produto;
+
+Produto p = new Produto();
+
+Console.WriteLine("Entre com os dados do produto: ");
+Console.Write("Nome: ");
+p.Nome = Console.ReadLine();
+
+Console.Write("Preço: ");
+p.Preco = double.Parse(Console.ReadLine());
+
+Console.Write("Quantidade: ");
+p.Quantidade = int.Parse(Console.ReadLine());
+
+Console.WriteLine();
+Console.WriteLine("Dados do produto: " + p);
+
+Console.WriteLine();    
+Console.Write("Digite o número dos produtos a serem adicionados: ");
+int qte = int.Parse(Console.ReadLine());
+p.AdicionarProdutos(qte);
+
+Console.WriteLine();
+Console.WriteLine("Dados atualizados: " + p);
+
+Console.WriteLine();
+Console.Write("Digite o número dos produtos a serem removidos: ");
+qte = int.Parse(Console.ReadLine());
+p.RemoverProdutos(qte);
+
+Console.WriteLine();
+Console.WriteLine("Dados atualizados: " + p);
